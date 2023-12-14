9f9f7363666223b5a4b85024214b21256266debb
Ahmed-Osama-Fawzy <ahmd.osama2611@mail.com>
2023-12-14 14:51:44 +0200

    Add Condtions for SJFNonPreemptive

--- Assignment.java
+++ Assignment.java
@@ -20,62 +20,53 @@ class Process {
 class SJFNonPreemptive {
     // The List of Processes not arranged Pure from user
     List<Process> Processes;
+    int SumArrival = 0;
+    int SumResponse = 0;
+    int SumTurnaround = 0;
+    int SizeNum;
     public SJFNonPreemptive(List<Process> Processes){
         // Set the value of the list
         this.Processes = Processes;
+        SizeNum = this.Processes.size();
         // Arrange the list depends on the value of the burst time for each process
-        Processes.sort(Comparator.comparingInt(p -> p.BurstTime));
-        /*
-        // Arrange the list depends on the value of the burst time for each process
-        for (int i = 0 ; i < Processes.size() ; i++){
-            if (i < Processes.size()-1 && Processes.get(i).ArrivalTime == Processes.get(i + 1).ArrivalTime) {
-                if (Processes.get(i).BurstTime > Processes.get(i+1).BurstTime) {
-                    // swapping the processes if you need to achieve the right arrange for access the cpu
-                    Collections.swap(Processes,i+1,i);
-                }
+        Processes.sort(Comparator.comparingInt(p -> p.ArrivalTime));
+        int CurrentTime = 0;
+        while (!Processes.isEmpty()) {
+            // Find the process with the shortest burst time among arrived processes
+            Process shortestJob = Processes.get(0);
+            for (Process P : Processes) {
+                if (P.ArrivalTime > CurrentTime) {break; /*Stop searching when you reach a P that hasn't arrived yet*/}
+                if (P.BurstTime < shortestJob.BurstTime) {shortestJob = P;}
             }
-        }
-        */
-        int Counter = 0 ;
-        for (Process P:Processes){
             // set the value of the waiting time for each process
-            P.Waiting += Counter - P.ArrivalTime;
+            shortestJob.Waiting += CurrentTime - shortestJob.ArrivalTime;
+            SumArrival += shortestJob.Waiting;
             // set the value of the response time for each process
-            P.Response += Counter - P.ArrivalTime;
-            // printing the chart of the algorithm
-            System.out.print(Counter + "  " + P.Name + " = " + P.BurstTime);
-            // increase the counter by the burst value of the terminated process
-            Counter += P.BurstTime;
-            System.out.print("  "  + Counter);
-            System.out.println();
-            // set the value of the turnaround time for each process
-            P.Turnaround = Counter - P.ArrivalTime;
-            //System.out.println(P.Waiting + " | " + P.Response + " | " + P.Turnaround);
+            shortestJob.Response += CurrentTime - shortestJob.ArrivalTime;
+            SumResponse += shortestJob.Response;
+            // Execute the shortest job
+            System.out.println("Executing process " + shortestJob.Name +
+                    " from time " + CurrentTime + " to " + (CurrentTime + shortestJob.BurstTime));
+            // Update current time and remove the executed process from the list
+            CurrentTime += shortestJob.BurstTime;
+            shortestJob.Turnaround = CurrentTime - shortestJob.ArrivalTime;
+            SumTurnaround += shortestJob.Turnaround;
+            Processes.remove(shortestJob);
+            System.out.println(shortestJob.Waiting + " " + shortestJob.Response + " " + shortestJob.Turnaround);
         }
+        System.out.println(SumArrival + " " + SumResponse + " " + SumTurnaround);
     }
     // calc the average of the turnaround time of the algorithm
     public float ATT() {
-        float Sum = 0;
-        for (Process P : Processes) {
-            Sum += P.Turnaround;
-        }
-        return (Sum / Processes.size());
+        return (SumTurnaround / (float)SizeNum);
     }
     // calc the average of the turnaround time of the algorithm
     public float AWT() {
-        float Sum = 0;
-        for (Process P : Processes) {
-            Sum += P.Waiting;
-        }
-        return (Sum / Processes.size());
+        return (SumArrival / (float)SizeNum);
     }
     // calc the average of the Response time of the algorithm
     public float ART() {
-        float Sum = 0;
-        for (Process P : Processes) {
-            Sum += P.Response;
-        }
-        return (Sum / Processes.size());
+        return (SumResponse / (float)SizeNum);
     }
 }
 
@@ -90,9 +81,9 @@ public class Assignment{
     public static void main(String[] args) {
         List<Process> processes = new ArrayList<>();
         processes.add(new Process("P1", 1,0,1,"Blue"));
-        processes.add(new Process("P2", 4,1,1,"BLack"));
-        processes.add(new Process("P3", 7,2,1,"DDDD"));
-        processes.add(new Process("P4", 5,3,1,"Red"));
+        processes.add(new Process("P2", 4,0,1,"Yellow"));
+        processes.add(new Process("P3", 3,0,1,"Green"));
+        processes.add(new Process("P4", 5,0,1,"Red"));
         SJFNonPreemptive X = new SJFNonPreemptive(processes);
         System.out.println("The Average of the waiting time is: " + X.AWT());
         System.out.println("The Average of the turnaround time is: " + X.ATT());
